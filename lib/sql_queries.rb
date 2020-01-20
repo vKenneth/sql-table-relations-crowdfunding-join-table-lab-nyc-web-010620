bb373475cf90259e18de17d4ddee08a2b69aeffa
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-20 00:17:40 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,3 +29,29 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "Write your SQL query Here"
 end
+
+
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal NUMERIC,
+start_date TEXT,
+end_date TEXT
+)
+;
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+)
+;
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount NUMERIC,
+user_id INTEGER,
+project_id INTEGER
+)
+;
